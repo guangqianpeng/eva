bf7a5c6701a4e8fe776d8de46567648ec7005b6f
frank <625727796@qq.com>
2018-01-07 16:03:36 +0800

    remove template

--- run.cc
+++ run.cc
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
     const uint8_t* data;
     std::unordered_map<
             Unit,
-            std::unique_ptr<Analyzer>,
+            Analyzer*,
             HashUnit> flowMap;
 
     while ((data = pcap_next(cap, &hdr)) != nullptr) {
